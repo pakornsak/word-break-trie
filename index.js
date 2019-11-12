0dc37a70b17be56cfa795d40fdf1a256000403b6
Pakornsak Supamongkonchaikul <pakornsak.sup@ascendcorp.com>
2019-11-12 11:21:54 +0700

    init project

---
+++ index.js
@@ -0,0 +1,22 @@
+function wordBreakTrie(s, hasWord, longestWordLength) {
+    const size = s.length;
+    if (size === 0) return [];
+  
+    const max = longestWordLength || size;
+    for (let i = max; i >= 1; i--) {
+      const word = s.substring(0, i);
+      const found = hasWord(word);
+      if (found) {
+        const ret = [word];
+  
+        const end = i === s.length;
+        const words = !end ? wordBreakTrie(s.substring(i), hasWord, max) : [];
+        ret.push(...words);
+        return ret;
+      }
+    }
+    return [s];
+  }
+  
+  module.exports = wordBreakTrie;
+  
